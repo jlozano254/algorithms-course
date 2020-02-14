20f3a4915f7704cba93d2215980ac7836a6ca4a1
Jesus Lozano <jesusemanuel.254@gmail.com>
2020-02-13 19:30:39 -0600

    feat: Adds fibonacci recursive with helpers

--- 3.-Recursion/fib_recursive.c
+++ 3.-Recursion/fib_recursive.c
@@ -2,14 +2,14 @@
 
 int fib(int n)
 {
-   if (n <= 1) return n;
-   return fib(n-1) + fib(n-2);
+  if (n <= 1) return n;
+  else return fib(n - 1) + fib(n - 2);
 }
 
 int main ()
 {
   int n;
-  scanf("%d", &n)
-  printf("%d", fib(n));
+  scanf("%d", &n);
+  printf("%d\n", fib(n));
   return 0;
 }
